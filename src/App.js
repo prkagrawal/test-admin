5b15bc50ecc269467cc2d0f74dfdef57fc826af6
Prince Agrawal <princeagrawalnew123@gmail.com>
2021-01-03 14:23:40 +0530

    fetched and display users from the api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Admin } from 'react-admin';
+import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 
 import './App.css';
@@ -9,7 +9,9 @@ const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 function App() {
   return (
     <div>
-      <Admin dataProvider={dataProvider} />
+      <Admin dataProvider={dataProvider}>
+        <Resource name="users" list={ListGuesser} />
+      </Admin>
     </div>
   );
 }
